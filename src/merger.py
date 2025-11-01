4a9076738dd366f0aa2106768c972b1a08938f7a
danieljohnconstantine-a11y <danieljohnconstantine@gmail.com>
2025-11-01 18:30:13 +1000

    Update merger.py

--- src/merger.py
+++ src/merger.py
@@ -3,7 +3,6 @@ from typing import List
 from .config import MERGE_KEYS
 from .utils import safe_concat_frames
 
-
 def merge_all(frames: List[pd.DataFrame], header_order: List[str]) -> pd.DataFrame:
     df = safe_concat_frames(frames, on_keys=MERGE_KEYS, how="outer")
     for col in header_order:
